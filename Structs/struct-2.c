34dd8a9cdd5a2848f81f16ad621e75f405ddd579
KConti05 <kauacontiero@gmail.com>
2025-05-14 17:31:03 -0300

    Update struct-2.c

--- Structs/struct-2.c
+++ Structs/struct-2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define TAM 5
 typedef struct
 {
   int dia;
@@ -10,6 +11,25 @@ typedef struct
   TData aniversario;
 } TPessoa;
 
-int main()
+int pessoasEmMes(int tamanho,TPessoa string[tamanho],int mes)
 {
   
+}
+int main()
+{
+  int i;  
+  TPessoa vPessoas[TAM];
+  for(i=0;i<TAM;i++)
+  {
+    printf("\nNome:\n");fgets(vPessoas[i].nome,TAM,stdin);
+    printf("\nDia do Aniversario:\n");scanf("%i",vPessoas[i].aniversario.dia);
+    printf("\nMes do Aniversario:\n");scanf("%i",vPessias[i].aniversario.mes);
+  }
+  printf("\n\nLista de Pessoas:\n");
+  for(i=0;i<TAM;i++)
+  {
+    printf("\nPessoa %i:\n",i+1);
+    printf("Nome: %s\n",vPessoas[i].nome);
+    printf("Data de ANiversario: %i/%i\n",vPessoas[i].aniversario.dia,vPessoas[i].aniversario.mes);
+  }
+}
