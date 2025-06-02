36bd96528be3dde491f454ebf09d7d72203603d7
KConti05 <kauacontiero@gmail.com>
2025-06-02 15:00:44 -0300

    Create quicksort.c

---
+++ sorts/quicksort.c
@@ -0,0 +1,30 @@
+/*É um método de ordenação que separa o vetor em pares de subvetores, um com os elementos menores que um 'compara', escolhido para rodar o vetor, e outro com os maiores que o 'compara', recursivamente até que o 
+vetor esteja ordenado. 
+Para isso usa-se uma função 'particiona' que divide o vetor e retorna o índice que marca a divisão e outra 'quickSort' que utiliza da particiona para dividir e organizar o vetor recursivamente.*/
+
+#include <stdio.h>
+
+int particiona(int *vet, int inicio, int fim)//para esta funcão é mais interessante usar inicio e fim do que o tamanho do vetor.
+{
+  int passa, compara=(vet[inicio]+vet[fim]+vet[(inicio+fim)/2])/3; //para o quicksort é interessante que o 'compara' seja uma media do vetor, para isso soma-se inicio, fim e meio e divide a soma por 3.
+  while(inicio<fim)
+  {
+    //procurando um elemento maior que o 'compara', do inicio para o fim:
+    while(inicio<fim && v[inicio]<=compara)
+    {
+      inicio+=1;
+    }
+    //procurando um elemento menor que o 'compara', do fim para o inicio:
+    while(inicio<fim && v[fim]>=compara)
+    {
+      fim-=1;
+    }
+    //realizando a troca:
+    passa=v[inicio];
+    v[inicio]=v[fim];
+    v[fim]=passa;
+  }
+}
+void quickSort(int *vet, int inicio, int fim)
+{
+}
