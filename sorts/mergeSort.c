2897778583ad2d1c0e150b85820234dbdbb7a4b8
KConti05 <kauacontiero@gmail.com>
2025-06-02 17:20:34 -0300

    Update mergeSort.c

--- sorts/mergeSort.c
+++ sorts/mergeSort.c
@@ -1,1 +1,7 @@
+/*É um método de ordenação que utiliza do "Separar e Conquistar".
+Ele divide o vetor em subvetores recursivamente até que cada elemento possua um vetor unitário próprio, então organiza esses subvetores até um vetor
+ordenado.*/
 
+#include <stdio.h>
+
+void mergeSort(
