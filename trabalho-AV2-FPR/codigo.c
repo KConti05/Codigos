05cee7af8bc06f1107552fd3fec5a5c5ed7c1461
KConti05 <kauacontiero@gmail.com>
2025-06-15 14:22:11 -0300

    Update codigo.c

--- trabalho-AV2-FPR/codigo.c
+++ trabalho-AV2-FPR/codigo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <strings.h>
+#include <string.h>
 //criação da variavel TInsumo
 typedef struct
 {
@@ -79,7 +79,7 @@ int transcreveArquivo(char *nomeArq, TInsumo *vCurso) //recebe o arquivo e o vet
 void calculaCpcCont(TInsumo *vCurso, float *vCpc) //recebe o vetor de cursos lidos e um vetor para armazenar os CPCs dos cursos
 {
   int i;
-  for(i=0;i<strlen(vCurso);i++) //roda o vetor cursos e armazena no vetor cpc o cpc do curso com indice(i) equivalente
+  for(i=0;i<30;i++) //roda o vetor cursos e armazena no vetor cpc o cpc do curso com indice(i) equivalente
     {
       vCpc[i]=((vCurso[i].notaEnade*20.0)+(vCurso[i].notaIdd*35.0)+(vCurso[i].doutores*15.0)+(vCurso[i].mestres*7.5)+(vCurso[i].regmTrabalho*7.5)+(vCurso[i].orgPedag*7.5)+(vCurso[i].infraestrutura*5.0)+(vCurso[i].amplAcademica*2.5))/100.0;
     }
@@ -89,7 +89,7 @@ float calculaIgcCont(TInsumo *vCurso, float *vCpc) //recebe os vetores vCurso e 
 {
    int i, totAlun=0; //totAlun sera usado no calculo da medio do igc
    float igc=0;
-   for(i=0;i<strlen(vCurso);i++) //roda os vetores progressivamente calculando igc e totAlun
+   for(i=0;i<30;i++) //roda os vetores progressivamente calculando igc e totAlun
    {
       igc+=vCpc[i]*vCurso[i].nAlunos;
       totAlun+=vCurso[i].nAlunos;
@@ -121,41 +121,41 @@ int calculaIgcFaixa(float igc)//recebe igc e retorna sua faixa
   }
 }
 //funcao para calculo de CPC faixa e qualidades:
-void calculaCpcFaixaQualid(float *vCpc, int *mFaixa) //preenche a matriz vFaixa rodando o de vCpcs e determinando a faixa de cada curso junto da qualidade(0-insatisfatorio,1-satisfatorio)
+void calculaCpcFaixaQualid(float *vCpc, int mFaixa[][2]) //preenche a matriz vFaixa rodando o de vCpcs e determinando a faixa de cada curso junto da qualidade(0-insatisfatorio,1-satisfatorio)
 {
   int i;
   //preenchendo vFaixa:
-  for(i=0;i<strlen(vCpc);i++)
+  for(i=0;i<30;i++)
   {
     if(vCpc[i]<0.945)
     {
-      vFaixa[i][0]=1;
-      vFaixa[i][1]=0; //insatisfatorio
+      mFaixa[i][0]=1;
+      mFaixa[i][1]=0; //insatisfatorio
     }
     else if(vCpc[i]<1.945)
     {
-      vFaixa[i][0]=2;
-      vFaixa[i][1]=0; //insatisfatorio
+      mFaixa[i][0]=2;
+      mFaixa[i][1]=0; //insatisfatorio
     }
     else if(vCpc[i]<2.945)
     {
-      vFaixa[i][0]=3;
-      vFaixa[i][1]=1; //satisfatorio
+      mFaixa[i][0]=3;
+      mFaixa[i][1]=1; //satisfatorio
     }
     else if(vCpc[i]<3.945)
     {
-      vFaixa[i][0]=4;
-      vFaixa[i][1]=1; //satisfatorio
+      mFaixa[i][0]=4;
+      mFaixa[i][1]=1; //satisfatorio
     }
     else
     {
-      vFaixa[i][0]=5;
-      vFaixa[i][1]=1; //satisfatorio
+      mFaixa[i][0]=5;
+      mFaixa[i][1]=1; //satisfatorio
     }
   }   
 }
 //funcao para exibir todos os cursos
-void exibeCursos(float igcCont, int igcFaixa, Tinsumo *vCurso, float *vCpc, int *mFaixa) //recebe os Cursos, os Cpcs dos cursos, o Igc(continuo e faixa) e a matriz com as faixas e qualidades dos cursos para exibi-los de forma organizada
+void exibeCursos(float igcCont, int igcFaixa, TInsumo *vCurso, float *vCpc, int mFaixa[][2]) //recebe os Cursos, os Cpcs dos cursos, o Igc(continuo e faixa) e a matriz com as faixas e qualidades dos cursos para exibi-los de forma organizada
 {
   int i, faixas;
   printf("\nCURSOS POR FAIXA DE QUALIDADE:\n");
@@ -164,13 +164,13 @@ void exibeCursos(float igcCont, int igcFaixa, Tinsumo *vCurso, float *vCpc, int 
   {
     printf("\nFaixa de Qualidade: %i\n",faixas);
     //Procurando faixa em mFaixa:
-    for(i=0;i<strlen(vCpc);i++)
+    for(i=0;i<30;i++)
     {
       //testando se a faixa e a requerida:
       if(mFaixa[i][1]==faixas)
       {
         //exibindo o curso a depender da qualidade:
-        if(faixa<3) //insatisfatorio
+        if(faixas<3) //insatisfatorio
         {
           printf("\nCodigo do Curso: %i;\nCPC Continuo: %.3f;\nCPC Faixa: %i;\nQualidade: INSATISFATORIO!\n",vCurso[i].codCurso,vCpc[i],mFaixa[i][0]);
         }
@@ -186,8 +186,8 @@ void exibeCursos(float igcCont, int igcFaixa, Tinsumo *vCurso, float *vCpc, int 
 //funcao para adicionar novos cursos:
 int adicionaCursos(char *nomeArq)
 {
-  char sContinua[5]="sim", sEscreve[10];
-  FILE* arq;
+  char sContinua[3]="sim", sEscreve[10];
+  FILE *arq;
   //abrindo arquivo:
   arq=fopen(nomeArq, "w");
   //testando se foi aberto:
@@ -196,7 +196,7 @@ int adicionaCursos(char *nomeArq)
     return 0;
   }
   //adicionando cursos:
-  while(sContinua=="sim")
+  while(strncmp(sContinua,"sim",3)==0)
   {
     printf("\n\nCodigo do Curso(4 digitos):\n");scanf("%s",sEscreve);
     fprintf(arq,"%s|",sEscreve);
@@ -218,7 +218,7 @@ int adicionaCursos(char *nomeArq)
     fprintf(arq,"%s|",sEscreve);
     printf("\nQuantidade de Alunos:\n");scanf("%s",sEscreve);
     fprintf(arq,"%s",sEscreve);
-    printf("\n\nDeseja adicionar mais cursos?"\n);scanf("%s",sContinua);
+    printf("\n\nDeseja adicionar mais cursos?\n");scanf("%s",sContinua);
   }
   //fechando arquivo:
   fclose(arq);
@@ -231,18 +231,18 @@ int main()
   //variaveis:
   int igcFaixa, anotCursos, mFaixa[30][2]; //mFaixa armazena na coluna 0 o valor da Faixa e na coluna 1 a qualidades(0-insatisfatorio, 1-satisfatorio), anotCursos recebera se o arquivo foi transcrito
   float igcCont, vCpc[30];
-  char sNomeArq[30], sUso[30];
+  char sNomeArq[30], sUso[15];
   TInsumo vCursos[30];
   //recebendo nome do arquivo:
-  printf("\nNome do Arquivo:\n");scanf("%s",nomeArq);
+  printf("\nNome do Arquivo:\n");scanf("%s",sNomeArq);
   //perguntando uso do arquivo:
-  while(sUso!="fim")
+  while(strncmp(sUso,"fim",15)!=0)
   {
     printf("\nQual o uso do arquivo?(digite 'adicionar' para adicionar novos cursos, 'exibir' para exibir cursos e 'fim' para encerrar)\n");scanf("%s",sUso); 
-    if(sUso=='exibir')
+    if(strncmp(sUso,"exibir",15)==0)
     {
       //transcrevendo arquivo para o vetor
-      anotCursos=transcreveCursos(sNomeArq, vCursos);
+      anotCursos=transcreveArquivo(sNomeArq, vCursos);
       //testando se arquivo foi aberto e executando
       if(anotCursos)
       {
@@ -260,10 +260,10 @@ int main()
         return 0;
       }
     }
-    else if(sUso=='adicionarr')
+    else if(strncmp(sUso,"adicionar",15)==0)
     {
       //preenchendo arquivo
-      anotCursos=adicionaCursos(sNomearq);
+      anotCursos=adicionaCursos(sNomeArq);
       if(!anotCursos)
       {
         printf("\nArquivo nao pode ser aberto!\n");
