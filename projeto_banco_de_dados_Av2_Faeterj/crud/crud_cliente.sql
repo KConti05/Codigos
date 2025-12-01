4464fd96a824a1d29d3c34932902517a0c1d2446
KConti05 <kauacontiero@gmail.com>
2025-12-01 18:33:21 -0300

    Fix SQL syntax for inserting multiple clients

--- projeto_banco_de_dados_Av2_Faeterj/crud/crud_cliente.sql
+++ projeto_banco_de_dados_Av2_Faeterj/crud/crud_cliente.sql
@@ -1,8 +1,7 @@
 USE falls_car;
 
 INSERT INTO cliente(nome,cpf_cnpj,email,senha_hash,cnh,telefone) 
-VALUES ("Thiago Tavares","00000000001","thiago@gmail.com","1234","00000000001","21900030003"),
-  ("Vitor Medeiros","0000000002","vitor@gmail.com","4321","00000000002","21900020002");
+VALUES ("Thiago Tavares","00000000001","thiago@gmail.com","1234","00000000001","21900030003"),("Vitor Medeiros","0000000002","vitor@gmail.com","4321","00000000002","21900020002");
 
 SELECT nome,cnh,email,telefone FROM cliente WHERE id=1;
 
