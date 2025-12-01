9e39412f6a140fd1de79a76a247aa96ddcb1e3de
KConti05 <kauacontiero@gmail.com>
2025-12-01 18:29:09 -0300

    Add CRUD operations for cliente table
    This SQL script performs CRUD operations on the 'cliente' table, including inserting, selecting, updating, and deleting records.

---
+++ projeto_banco_de_dados_Av2_Faeterj/crud/crud_cliente.sql
@@ -0,0 +1,17 @@
+USE falls_car;
+
+INSERT INTO cliente(nome,cpf_cnpj,email,senha_hash,cnh,telefone) 
+VALUES ("Thiago Tavares","00000000001","thiago@gmail.com","1234","00000000001","21900030003"),
+  ("Vitor Medeiros","0000000002","vitor@gmail.com","4321","00000000002","21900020002");
+
+SELECT nome,cnh,email,telefone FROM cliente WHERE id=1;
+
+UPDATE cliente
+SET telefone="21900030003"
+WHERE id=1;
+
+SELECT nome,cnh,email,telefone FROM cliente WHERE id=2;
+DELETE FROM cliente WHERE id=2;
+SELECT * FROM cliente;
+
+DELETE FROM cliente;
