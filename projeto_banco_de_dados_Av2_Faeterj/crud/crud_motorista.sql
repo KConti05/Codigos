4def7efa8ead6b1822ef8c9961b4062826cf7e1e
KConti05 <kauacontiero@gmail.com>
2025-12-02 10:55:56 -0300

    Update crud_motorista.sql

--- projeto_banco_de_dados_Av2_Faeterj/crud/crud_motorista.sql
+++ projeto_banco_de_dados_Av2_Faeterj/crud/crud_motorista.sql
@@ -2,8 +2,7 @@ USE falls_car;
 
 INSERT INTO funcionario(nome,cpf,email,senha_hash,funcao) 
   VALUES ("Wagner Zanco","00000000029","zanco@gmail.com","7890","motorista"),("Eduardo Barreto","00000000030","barreto@gmail.com","9001","motorista");
-INSERT INTO motorista(id_funcionario,nome,cnh,diaria) 
-  VALUES (1,"Wagner Zanco","00000000029",124.15);
+INSERT INTO motorista(id_funcionario,nome,cnh,diaria) VALUES (1,"Wagner Zanco","00000000029",124.15);
 
 UPDATE FROM motorista
 SET id_funcionario=2,nome="Eduardo Barreto",cnh="00000000030",diaria=169.98)
