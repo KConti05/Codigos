b0a97c53c8c1a07c965876e72619703c1a131d75
KConti05 <kauacontiero@gmail.com>
2025-12-02 17:33:23 -0300

    Add phone number to loja insertion query

--- projeto_banco_de_dados_Av2_Faeterj/crud/crud_carro.sql
+++ projeto_banco_de_dados_Av2_Faeterj/crud/crud_carro.sql
@@ -1,7 +1,7 @@
 USE falls_car;
 
 INSERT INTO modelo(nome,marca,ano,categoria,preco_padrao) VALUES ("Onix","Chevrolet","2020","Hatch",130.29);
-INSERT INTO loja(nome,endereco,cidade,uf,telefone) VALUES ("Loja Suvaco Seco","Rua SB, numero 413","Carpazinha","RS");
+INSERT INTO loja(nome,endereco,cidade,uf,telefone) VALUES ("Loja Suvaco Seco","Rua SB, numero 413","Carpazinha","RS","21941300413");
 INSERT INTO carro(id_modelo,id_loja,placa,automatico,valor,status) VALUES (1,1,"ABC1234",TRUE,150.29,"livre");
 
 SELECT placa,automatico,valor FROM carro WHERE status="livre";
